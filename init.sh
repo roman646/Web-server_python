f9023e554862314e5a034ad5fb3e342bf5817a5e
roman646 <66514752+roman646@users.noreply.github.com>
2021-05-05 15:47:23 +0300

    Update init.sh
    version2

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ sudo mkdir ../web/etc
 sudo mkdir ../web/public/img
 sudo mkdir ../web/public/css
 sudo mkdir ../web/public/js
-sudo cp nginx.config ../web/nginx.config
+sudo cp nginx.conf ../web/nginx.config
 sudo rm ../../../../etc/nginx/ngonx.config
 sudo ln -s nginx.config ../../../../etc/nginx
