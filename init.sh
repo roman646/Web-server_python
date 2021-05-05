271b56fef3f59b09504085ac94c8edcafe743ae6
roman646 <66514752+roman646@users.noreply.github.com>
2021-05-05 15:13:40 +0300

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,12 @@
+
+sudo mkdir web
+sudo cd web
+sudo mkdir public
+sudo mkdir uploads
+sudo mkdir etc
+sudo cd public
+sudo mkdir img
+sudo mkdir css
+sudo mkdir js
+sudo rm ../../../../etc/nginx/ngonx.config
+sudo ln -s nginx.config ../../../../etc/nginx/ngonx.config
